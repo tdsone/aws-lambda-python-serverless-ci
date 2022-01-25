17c6bc6c5cee321ad0a6587e6c6e4b447515d3ff
Tdsone <timon_schneider@outlook.de>
2022-01-25 12:17:24 +0000

    test pandas import

--- handler.py
+++ handler.py
@@ -1,10 +1,12 @@
 import json
-
+import pandas as pd
 
 def hello(event, context):
     body = {
         "message": "Go Serverless v2.0! Your function executed successfully!",
         "input": event,
     }
+
+    test = pd.DataFrame([])
 
     return {"statusCode": 200, "body": json.dumps(body)}
